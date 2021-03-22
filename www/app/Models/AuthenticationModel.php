2bad6284135986669b56b7e4d58b5ccf6383aed9
DA\suwit <suwit@da.co.th>
2021-03-22 10:42:28 +0700

    -

--- www/app/Models/AuthenticationModel.php
+++ www/app/Models/AuthenticationModel.php
@@ -16,17 +16,7 @@ class AuthenticationModel extends Model
 
     public function getToken()
     {
-        $dsn = 'mysql:dbname=' . $this->db->database . ';host=' . $this->db->hostname;
-        $username = $this->db->username;
-        $password = $this->db->password;
         \OAuth2\Autoloader::register();
-
-        // $dsn is the Data Source Name for your database, for exmaple "mysql:dbname=my_oauth2_db;host=localhost"
-        $storage = new \OAuth2\Storage\Pdo(array(
-            'dsn' => $dsn,
-            'username' => $username,
-            'password' => $password
-        ));
 
         // Pass a storage object or array of storage objects to the OAuth2 server class
         $server = new \OAuth2\Server($this->db);
