63dfcd4bca321f358337fae6609e23b10f64be35
DA\suwit <suwit@da.co.th>
2021-03-22 10:43:46 +0700

    -

--- www/app/Models/AuthenticationModel.php
+++ www/app/Models/AuthenticationModel.php
@@ -16,16 +16,26 @@ class AuthenticationModel extends Model
 
     public function getToken()
     {
+        $dsn = 'mysql:dbname=' . $this->db->database . ';host=127.0.0.1';
+        $username = $this->db->username;
+        $password = $this->db->password;
         \OAuth2\Autoloader::register();
 
+        // $dsn is the Data Source Name for your database, for exmaple "mysql:dbname=my_oauth2_db;host=localhost"
+        $storage = new \OAuth2\Storage\Pdo(array(
+            'dsn' => $dsn,
+            'username' => $username,
+            'password' => $password
+        ));
+
         // Pass a storage object or array of storage objects to the OAuth2 server class
-        $server = new \OAuth2\Server($this->db);
+        $server = new \OAuth2\Server($storage);
 
         // Add the "Client Credentials" grant type (it is the simplest of the grant types)
-        $server->addGrantType(new \OAuth2\GrantType\ClientCredentials($this->db));
+        $server->addGrantType(new \OAuth2\GrantType\ClientCredentials($storage));
 
         // Add the "Authorization Code" grant type (this is where the oauth magic happens)
-        $server->addGrantType(new \OAuth2\GrantType\AuthorizationCode($this->db));
+        $server->addGrantType(new \OAuth2\GrantType\AuthorizationCode($storage));
 
         $server->handleTokenRequest(\OAuth2\Request::createFromGlobals())->send();
     }
