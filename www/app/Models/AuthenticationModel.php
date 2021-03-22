cbdff76a7676270126c0aa6c308425fb793f4575
DA\suwit <suwit@da.co.th>
2021-03-22 10:46:19 +0700

    -

--- www/app/Models/AuthenticationModel.php
+++ www/app/Models/AuthenticationModel.php
@@ -16,7 +16,7 @@ class AuthenticationModel extends Model
 
     public function getToken()
     {
-        $dsn = 'mysql:dbname=' . $this->db->database . ';host=127.0.0.1';
+        $dsn = 'mysql:host=127.0.0.1;port=8080;dbname=' . $this->db->database;
         $username = $this->db->username;
         $password = $this->db->password;
         \OAuth2\Autoloader::register();
